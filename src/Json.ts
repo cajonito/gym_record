8528f82fdf661323cc9a5341e773f2e23aac110a
nk <nkitgamesake@gmail.com>
2020-03-11 20:19:15 +0900

    Jsonクラスの機能拡張（ルート取得、ルートが配列の場合に対応） #19

--- src/Json.ts
+++ src/Json.ts
@@ -1,9 +1,11 @@
 export class Json {
-  object: { [key: string]: any };
+  object: { [key: string]: any } | any[];
   constructor(object: { [key: string]: any }) {
     this.object = object;
   }
-  get(path: string): any {
+  get(path?: string): any {
+    if (path === undefined) return this.object;
+
     let lookup: any = Object.assign({}, this.object);
     const keys: string[] = path.split('.');
     for (let i = 0; i < keys.length; i++) {
--- tests/Json.spec.ts
+++ tests/Json.spec.ts
@@ -2,6 +2,8 @@ import { Json } from '../src/Json';
 
 test('get', () => {
   let json: Json
+
+  // ルートがObjectの場合
   json = new Json({
     'a': 'A',
     'b': {
@@ -28,6 +30,24 @@ test('get', () => {
   expect(json.get('b.d.100.h.2.1')).toBe(undefined);
   expect(json.get('.....')).toBe(undefined);
 
+  // 引数無しだとルートを取得する
+  json = new Json({
+    'a': {
+      'b': 'B'
+    }
+  })
+  expect(json.get()).toStrictEqual({
+    'a': {
+      'b': 'B'
+    }
+  })
+
+  // ルートが配列の場合
+  json = new Json([1, 2, 3])
+  expect(json.get('0')).toBe(1);
+  expect(json.get()).toStrictEqual([1, 2, 3]);
+
+  // 異常系
   json = new Json({});
   expect(json.get('some_key')).toBe(undefined);
 });
