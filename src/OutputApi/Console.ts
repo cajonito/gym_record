ac37f4a40f45b4195691d310971ad96025511202
nk <nkitgamesake@gmail.com>
2020-03-19 21:28:32 +0900

    デバッグメッセージ送信関数をAPIに追加 #20

--- src/OutputApi/Console.ts
+++ src/OutputApi/Console.ts
@@ -2,8 +2,13 @@ import { OutputApi } from '../OutputApi'
 import { Json } from '../Json'
 
 export class Console extends OutputApi {
-  sendMessage(text: string) {
+  sendDebugMessage(text: string, blocks?: Json) {
+    this.sendMessage(text, blocks);
+  }
+
+  sendMessage(text: string, blocks?: Json) {
     console.log(text);
+    console.log(JSON.stringify(blocks));
   }
 
   sendEphemeral(userId: String, text: String, blocks?: Json) {
